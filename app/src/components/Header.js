840583f2ac7705d5c46e4419ead41e1975a0942b
nnlinh97 <nnlinh97it@gmail.com>
2018-12-01 21:47:47 +0700

    fix css modal, xu li toggle, remove modal

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+// import './../styles/Header.css'
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: 'none'
+        }
+    }
+
+    toggleModal = () => {
+        this.setState({
+            modal: 'block'
+        })
+    }
+
+    removeModal = () => {
+        this.setState({
+            modal: 'none'
+        })
+    }
 
     preventDefault = (e) => {
         e.preventDefault();
@@ -12,6 +31,7 @@ class Header extends Component {
         e.preventDefault();
         this.props.history.push('/');
     }
+
     render() {
         const { profile } = this.props;
         let avatar = "https://tinyurl.com/yapenv5f";
@@ -19,12 +39,12 @@ class Header extends Component {
             avatar = profile.avatarURL;
         }
         return (
-            <div className="bg-white" style={{position: 'fixed', width: '100%', zIndex: 1}}>
+            <div className="bg-white" style={{ position: 'fixed', width: '100%', zIndex: 1 }}>
                 <div className="container mx-auto flex flex-col lg:flex-row items-center py-4">
                     <nav className="w-full lg:w-2/5">
                         <a onClick={(e) => this.preventDefault(e)} href="" className="text-grey-darker text-sm mr-4 font-semibold pb-6 border-b-2 border-solid border-transparent no-underline hover:text-teal hover:border-teal hover:no-underline">
                             <i className="fa fa-home fa-lg" /> Home</a>
-                        <a onClick={(e) => this.preventDefault(e)} style={{marginLeft: '2rem'}} href="" className="text-grey-darker text-sm mr-4 font-semibold pb-6 border-b-2 border-solid border-transparent no-underline hover:text-teal hover:border-teal hover:no-underline">
+                        <a onClick={(e) => this.preventDefault(e)} style={{ marginLeft: '2rem' }} href="" className="text-grey-darker text-sm mr-4 font-semibold pb-6 border-b-2 border-solid border-transparent no-underline hover:text-teal hover:border-teal hover:no-underline">
                             <i className="fa fa-bell fa-lg" /> Notifications</a>
                         {/* <a href="#" className="text-grey-darker text-sm mr-4 font-semibold pb-6 border-b-2 border-solid border-transparent no-underline hover:text-teal hover:border-teal hover:no-underline">
                             <i className="fa fa-envelope fa-lg" /> Messages
@@ -38,7 +58,7 @@ class Header extends Component {
                     <div className="w-full lg:w-2/5 flex lg:justify-end">
                         <div className="mr-4 relative">
                             <input type="text" className="bg-grey-lighter h-8 px-4 py-2 text-xs w-48 rounded-full" placeholder="Search Twitter" />
-                            <span style={{bottom: '6px'}} className="flex items-center absolute pin-r pin-y mr-3">
+                            <span style={{ bottom: '6px' }} className="flex items-center absolute pin-r pin-y mr-3">
                                 <i className="fa fa-search text-grey" />
                             </span>
                         </div>
@@ -58,21 +78,21 @@ class Header extends Component {
                             </div>
                         </div>
                         <div>
-                            <button className="bg-teal hover:bg-teal-dark text-white font-medium py-2 px-4 rounded-full">
+                            <button onClick={this.toggleModal} className="bg-teal hover:bg-teal-dark text-white font-medium py-2 px-4 rounded-full">
                                 Tweet
                             </button>
-                            <div className="modal fade" id="myModal" role="dialog">
+                            <div className="modal fade" id="myModal" role="dialog" style={{ display: this.state.modal }}>
                                 <div className="modal-dialog">
                                     <div className="modal-content">
-                                        <div className="modal-header text-center">
-                                            <button type="button" className="close" data-dismiss="modal">&times;</button>
-                                            <h4 className="modal-title">Compose new Tweet</h4>
+                                        <div className="modal-header text-center" style={{ height: '50px' }}>
+                                            <button onClick={this.removeModal} type="button" className="close" data-dismiss="modal">&times;</button>
+                                            <h4 className="modal-title" style={{ paddingTop: '11px' }}>Compose new Tweet</h4>
                                         </div>
                                         <hr />
                                         <div className="modal-body">
                                             <div className="row colorRow">
                                                 <div className="col-sm-2 avataImage">
-                                                    <img className="imageCol Avatar--size32 user-avatar-img avatar circle" src="https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png" alt="thanhhue"/>
+                                                    <img className="imageCol Avatar--size32 user-avatar-img avatar circle" src={avatar} alt="thanhhue" />
 
                                                 </div>
                                                 <div className="col-sm-10">
@@ -82,9 +102,9 @@ class Header extends Component {
                                             <div className="row footer-image " >
                                                 <div className="col-sm-2"></div>
                                                 <div className="col-sm-10 imageRow">
-                                                    <label htmlFor="image" ><i className="icon-picture" title="Add photo"></i></label>
-                                                    <input id="image" type="file"  name="image" /> 
-                                                </div>  
+                                                    <label htmlFor="image" style={{ fontSize: '25px' }}><i className="fa fa-image" title="Add photo"></i></label>
+                                                    <input id="image" type="file" name="image" />
+                                                </div>
                                             </div>
                                         </div>
                                         <div className="modal-footer ">
