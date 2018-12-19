dd6fb696f14ffef732c02c561fe7e107aae56d46
TRANDINHHUAN <dinhhuantran3010@gmail.com>
2018-12-19 19:25:50 +0700

    create register page

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import NotFoundPage from './pages/NotFoundPage';
 import Followers from './pages/Followers';
 import Following from './pages/Following';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 class App extends Component {
@@ -17,6 +18,8 @@ class App extends Component {
             <Route exact path='/:username/followers' component={Followers} />
             <Route exact path='/:username/followings' component={Following} />
             <Route exact path='/' component={Login} />
+            <Route exact path='/account/register' component={Register} />
+            
             <Route path='' component={NotFoundPage} />
           </Switch>
         </div>
--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -1,14 +1,11 @@
+
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom'
 
-class Login extends Component {
-
-    clickToTwitter = () => {
-        this.props.history.push('/nnlinh97');
-    } 
-    clickToRegister = () => {
-        this.props.history.push('account/register')
-    }  
+class Register extends Component {
+    clickToSignIn = () => {
+        this.props.history.push('/');
+    }
     render() {
         return (
             <div className="limiter">
@@ -16,11 +13,11 @@ class Login extends Component {
                     <div className="wrap-login100">
                         <form className="login100-form validate-form p-l-55 p-r-55 p-t-178">
                             <span className="login100-form-title">
-                                Sign In
+                                Create New Wallet
                             </span>
                             <div className="form-group">
                                 <div className="label">
-                                    <label htmlFor="" className="text-uppercase">Private key</label>
+                                    <label htmlFor="" className="text-uppercase">Enter a password</label>
                                 </div>
                                 <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
                                     <input className="input100" type="text" name="username" placeholder="Private key" />
@@ -28,29 +25,22 @@ class Login extends Component {
                                 </div>
                             </div>
 
-                            {/* <div className="wrap-input100 validate-input" data-validate="Please enter password">
-                                <input className="input100" type="password" name="pass" placeholder="Password" />
-                                <span className="focus-input100" />
-                            </div> */}
-                            <div className="text-right p-t-13 p-b-23">
-                                <span className="txt1">
-                                    Forgot &nbsp;
-                                </span>
-                                <a href="#" className="txt2">
-                                    Username / Password
-                                </a>
-                            </div>
+
+                            <br />
                             <div className="container-login100-form-btn">
                                 <button onClick={this.clickToTwitter} className="login100-form-btn">
-                                    Sign in
+                                    Create account
                                 </button>
+                            </div>
+                            <div>
+                                <br />
                             </div>
                             <div className="flex-col-c p-t-30 p-b-40">
                                 <span className="txt1 p-b-9">
-                                    Don’t have an account?&nbsp;
+                                    Have an account? &nbsp;
                                 </span>
-                                <a  onClick={this.clickToRegister} href="" className="txt3">
-                                    Sign up now
+                                <a  onClick={this.clickToSignIn} href="" className="txt3">
+                                    Sign in
                                 </a>
                             </div>
                         </form>
@@ -60,5 +50,4 @@ class Login extends Component {
         );
     }
 }
-
-export default withRouter(Login);
+export default withRouter(Register);
