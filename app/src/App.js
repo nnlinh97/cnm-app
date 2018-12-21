8efdda7564496d28ae1e403d100696d9df50445c
TRANDINHHUAN <dinhhuantran3010@gmail.com>
2018-12-21 14:34:55 +0700

    Them router user

--- app/src/App.js
+++ app/src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
             <Route exact path='/login' component={Login} />
             <Route exact path='/create' component={CreateAccount} />
             <Route exact path='/payment' component={Payment} />
+            <Route exact path='/users/:id' component={Profile} />
             {/* <Route exact path='/:username' component={Profile} />
             <Route exact path='/:username/followers' component={Followers} />
             <Route exact path='/:username/followings' component={Following} />
