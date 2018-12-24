25aa37f8424683646fe2ddab556c7aee5dec4761
nnlinh97 <nnlinh97it@gmail.com>
2018-12-24 09:45:18 +0700

    fix bug menutop, post

--- app/src/components/Post.js
+++ app/src/components/Post.js
@@ -56,7 +56,6 @@ class Post extends Component {
 
     componentWillUnmount() {
         document.removeEventListener('mousedown', this.handleClickOutside);
-
     }
 
     setWrapperRef = (node) => {
@@ -70,7 +69,6 @@ class Post extends Component {
     }
     render() {
         const {user} = this.state;
-        console.log(user);
         const { post } = this.props;
         const cssModal = this.state.detail ? "block" : "none";
         let avatar = "https://tinyurl.com/yapenv5f";
@@ -272,25 +270,25 @@ class Post extends Component {
                                     {post.liked ? <i className="fa fa-heart fa-lg mr-2" /> : <i className="fa fa-heart-o fa-lg mr-2" />}
                                     {post.likes}
                                 </a> */}
-                                <a class="facebook-reaction">
-                                    <span class="like-btn">
-                                        <span class="like-btn-emo like-btn-default"></span>
-                                        <span class="like-btn-text">Like</span>
-                                        <ul class="reactions-box">
-                                            <li class="reaction reaction-like" data-reaction="Like"></li>
-                                            <li class="reaction reaction-love" data-reaction="Love"></li>
-                                            <li class="reaction reaction-haha" data-reaction="HaHa"></li>
-                                            <li class="reaction reaction-wow" data-reaction="Wow"></li>
-                                            <li class="reaction reaction-sad" data-reaction="Sad"></li>
-                                            <li class="reaction reaction-angry" data-reaction="Angry"></li>
+                                <a className="facebook-reaction">
+                                    <span className="like-btn">
+                                        <span className="like-btn-emo like-btn-default"></span>
+                                        <span className="like-btn-text">Like</span>
+                                        <ul className="reactions-box">
+                                            <li className="reaction reaction-like" data-reaction="Like"></li>
+                                            <li className="reaction reaction-love" data-reaction="Love"></li>
+                                            <li className="reaction reaction-haha" data-reaction="HaHa"></li>
+                                            <li className="reaction reaction-wow" data-reaction="Wow"></li>
+                                            <li className="reaction reaction-sad" data-reaction="Sad"></li>
+                                            <li className="reaction reaction-angry" data-reaction="Angry"></li>
                                         </ul>
                                     </span>
-                                    <div class="like-stat">
-                                        <span class="like-emo">
-                                            <span class="like-btn-like"></span>
+                                    {/* <div className="like-stat">
+                                        <span className="like-emo">
+                                            <span className="like-btn-like"></span>
                                         </span>
                                         <span class="like-details">Arkaprava Majumder and 1k others</span>
-                                    </div>
+                                    </div> */}
                                 </a>
                             {/* </span> */}
                         {/* </div> */}
