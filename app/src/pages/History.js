43eb96925f1124a0325c2c4442360749caf03a28
TRANDINHHUAN <dinhhuantran3010@gmail.com>
2018-12-25 22:47:44 +0700

    Set cung css hien thi trang hien tai

--- app/src/pages/History.js
+++ app/src/pages/History.js
@@ -121,18 +121,18 @@ class History extends Component {
                                 </tbody>
                             </table>
                         </div>
-                        <br/>
+                        <br />
                         <div class="pagination">
                             <a href="#">&laquo;</a>
                             <a href="#">1</a>
-                            <a href="#">2</a>
+                            <a href="#" class="active">2</a>
                             <a href="#">3</a>
                             <a href="#">4</a>
                             <a href="#">5</a>
                             <a href="#">6</a>
                             <a href="#">&raquo;</a>
                         </div>
-                        <br/>
+                        <br />
                     </div>
 
                 </div>
